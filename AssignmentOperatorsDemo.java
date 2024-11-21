e0406fc97c2e59074a1f3d044e391e1a4235d9b8
mosesaio <ayoaade1@gmail.com>
2024-11-21 11:25:54 -0500

    Commit the projects worked on.

---
+++ AssignmentOperatorsDemo.java
@@ -0,0 +1,14 @@
+public class AssignmentOperatorsDemo {
+    public static void main(String[] args) {
+        int j,  k;
+        j = 10;  // j gets the value of 10.
+        j = 5; // j gets the value oof 5. The previous value is overwritten.
+        k = j; // k gets the value 5.
+        System.out.println("j is :" + j);
+        System.out.println("k is :" + k);
+
+        k = j = 10; // (k= (j=10))
+        System.out.println("j is :" + j);
+        System.out.println("k is :" + k);
+    }
+}
